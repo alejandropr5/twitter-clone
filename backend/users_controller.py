1286fa146fda74e19ef740cdc4ca3a05aad36d72
alejandropr05 <alejandropr05@hotmail.com>
2023-10-05 01:19:45 -0500

    Implement get_github_user_data function

--- backend/users_controller.py
+++ backend/users_controller.py
@@ -1,28 +1,51 @@
 # from typing import Annotated
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Request
 from fastapi.responses import RedirectResponse
 import requests
 
+import logging
 
-GITHUB_TOKEN_ENDPOINT = "https://github.com/login/oauth/access_token"
-CLIENT_ID = "5b65a4ee4e9e984f6c1a"
-CLIENT_SECRET = "11a4beac2fa8c67e23a91da5bcb2d907914413bd"
+logging.basicConfig(level=logging.DEBUG,
+                    format="\n[%(levelname)s] %(asctime)s: %(message)s")
+
+
+class GitHub():
+    TOKEN_ENDPOINT = "https://github.com/login/oauth/access_token"
+    USER_ENDPOINT = "https://api.github.com/user"
+    CLIENT_ID = "5b65a4ee4e9e984f6c1a"
+    CLIENT_SECRET = "11a4beac2fa8c67e23a91da5bcb2d907914413bd"
 
 
 async def get_github_access_token(code: str):
-    url_path = (f"{GITHUB_TOKEN_ENDPOINT}?client_id={CLIENT_ID}"
-                f"&client_secret={CLIENT_SECRET}&code={code}")
+    url_path = (f"{GitHub.TOKEN_ENDPOINT}?client_id={GitHub.CLIENT_ID}"
+                f"&client_secret={GitHub.CLIENT_SECRET}&code={code}")
     headers = {
         "Accept": "application/json"
     }
-    response = requests.request(method="POST", url=url_path, headers=headers)
-
+    response = requests.request(method="POST",
+                                url=url_path,
+                                headers=headers)
     try:
         data = response.json()
     except requests.exceptions.JSONDecodeError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    return data
+
+
+async def get_github_user_data(token: dict):
+    headers = {
+        "Authorization": f"{token['token_type']} {token['access_token']}"
+    }
+    response = requests.request(method="GET",
+                                url=GitHub.USER_ENDPOINT,
+                                headers=headers)
+    # try:
+    data = response.json()
+    # except requests.exceptions.JSONDecodeError:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
     return data
 
@@ -32,9 +55,12 @@ router = InferringRouter()
 
 @cbv(router)
 class UsersController:
-    @router.get("/login")
-    async def login(self, code: str):
-        data = await get_github_access_token(code)
+    @router.get("/github_login")
+    async def github_login(self, code: str, request: Request):
+        token = await get_github_access_token(code)
+        user_data = await get_github_user_data(token)
 
-        print("DEBUG:", data)
-        return RedirectResponse("http://localhost:3000")
+        logging.debug(token)
+        logging.debug(user_data)
+
+        return RedirectResponse(request.headers["referer"])
