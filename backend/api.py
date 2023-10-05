e417a66bc051faa162c584862250535feb9a7036
alejandropr05 <alejandropr05@hotmail.com>
2023-10-04 22:58:23 -0500

    Remove unused code

--- backend/api.py
+++ backend/api.py
@@ -14,31 +14,9 @@ app.add_middleware(
 )
 
 
-# @app.on_event("startup")
-# def load_model():
-#     """This function will run once
-#     when the application starts up"""
-#     print("Loading the model...")
-#     framework = Framework.sklearn
-#     model = ModelLoader(
-#         path=models_path[framework.value],
-#         framework=framework,
-#         labels=["setosa", "versicolor", "virginica"],
-#         name="iris_model",
-#         version=1.0,
-#     )
-#     print("model loaded successfully!")
-#     app.state.model = model
-
-
 @app.on_event("shutdown")
 def shutdown_event():
     print("Shutting down the application...")
 
 
 app.include_router(users_router, tags=["users"], prefix="/users")
-
-
-@app.get("/hello_world")
-def hello_world():
-    return {"message": "Hello World from the API"}
